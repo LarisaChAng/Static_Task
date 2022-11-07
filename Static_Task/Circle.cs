787130a4ff210dae4757d7bd5fae547f44d0c833
LarisaChAng <LChS81@mail.ru>
2022-11-07 15:00:26 +0800

    Добавьте файлы проекта.

---
+++ Static_Task/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Task
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2 * r * Math.PI;
+        }
+        static public double GetSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public string GetAreaPoint(double r, double x0, double y0, double x1, double y1)
+        {
+            double x = Math.Abs(x1 - x0);
+            double y = Math.Abs(y1 - y0);
+
+            if (x <= r && y <= r) 
+                return "Точка внутри окружности";
+            else
+                return "Точка не внутри окружности";
+        }
+    }
+}
---
+++ Static_Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Методы определяют: длину и площадь окружности; принадлежность точки кругу
+            Console.WriteLine("Введите радиус и координаты центра окружности:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double x0 = Convert.ToDouble(Console.ReadLine());            
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты точки:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            double length = Circle.GetLength(r);
+            double square = Circle.GetSquare(r);            
+            string areaPoint = Circle.GetAreaPoint(r, x0, y0, x1, y1);
+
+            Console.WriteLine($"Длина = {length:f2} \nПлощадь = { square:f2} \n{ areaPoint}");            
+            Console.ReadKey();
+        }
+    }
+}
